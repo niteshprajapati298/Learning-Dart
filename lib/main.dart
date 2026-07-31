6d144cc89d9eff06ccd96735161d5ea5116d9f63
niteshprajapati298 <niteshkmrprajapati298@gmail.com>
2026-07-31 23:09:04 +0530

    Add getFullName fumction and print result in MyApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
+String getFullName(String firstName , String lastName){
+  return "$firstName $lastName";
+}
 void main() {
-  
   runApp(const MyApp());
 }
 
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+     String ans = getFullName('Nitesh', 'Prajapati');
+  print(ans);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
